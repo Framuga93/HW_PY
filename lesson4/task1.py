c27e0781a10ac57b1e7e8584c34498a9c6f1bca3
Framuga93 <alekseydemochkin93@gmail.com>
2022-09-18 23:31:19 +0300

    add hw l4

---
+++ lesson4/task1.py
@@ -0,0 +1,14 @@
+# Вычислить число c заданной точностью d
+# Пример:
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+def toFixed(numObj, digits=0):
+    return f"{numObj:.{digits}f}"
+
+d = 0.001
+n = 3.1415926535
+i = 0
+while d < 1:
+    d *= 10
+    i += 1
+print(toFixed(n,i))
